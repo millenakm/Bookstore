2d200adeac49177aeec5c434c5c1bb355b44f47b
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-14 13:22:59 -0300

    dados

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next){//rota (localhost:porta/list-users)
+	fs.readFile(__dirname + '/../db/data.json', 'utf8', function(err, data){
+		var dados = JSON.parse(data);
+		res.render('users', {param: dados});
+		// res.send(dados[1].nome);
+	});
 });
 
 module.exports = router;
