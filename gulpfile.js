bf74d8bd24ba5099eee426d61da78f43d3355dc1
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-16 10:53:02 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 
 gulp.task('bootstrap', function(){
-	console.log('bootstrap');
-	gulp.src('./node_modules/bootstrap/**/*')
-	.pipe(gulp.dest('./bootstrap'));
+	gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css');
+	.pipe(gulp.dest('./public/stylesheets'));
+
+	gulp.src('./node_modules/bootstrap/dist/fonts/*');
+	.pipe(gulp.dest('./public/fonts'));
+
+	gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js');
+	.pipe(gulp.dest('./public/javascripts'));
 });
