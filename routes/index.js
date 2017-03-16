109b77ed8bc58521fe1fc2a9b823e69deb01f3b1
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-16 10:54:58 -0300

    rotas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var CodeGenerator = require('node-code-generator');
+var generator = new CodeGenerator();
+var pattern = '*****';
+var howMany = 38;
+var options = {};
+// Generate an array of random unique codes according to the provided pattern: 
+var codes = generator.generateCodes(pattern, howMany, options);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Padaria do João' });
+  res.render('index', { title: codes });
 });
 
 module.exports = router;
