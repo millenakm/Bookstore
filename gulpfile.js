78f142b46185d15a811df4f3e2153ef0e9906611
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-17 16:29:49 -0300

    less

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
 gulp.task('bootstrap', function(){
-	gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css');
+	console.log(path.join(__dirname));
+	
+	gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
 	.pipe(gulp.dest('./public/stylesheets'));
 
-	gulp.src('./node_modules/bootstrap/dist/fonts/*');
+	gulp.src('./node_modules/bootstrap/dist/fonts/*')
 	.pipe(gulp.dest('./public/fonts'));
 
-	gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js');
+	gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
 	.pipe(gulp.dest('./public/javascripts'));
+
 });
+
+gulp.task('less', function () {
+	return gulp.src('./less/*.less')
+	.pipe(less())
+	.pipe(gulp.dest('./public/stylesheets'));
+});
+
