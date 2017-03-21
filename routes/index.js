6581a5f9f470b3e09acd8204f4dbba204c2d9244
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-21 15:19:43 -0300

    rota

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var jsdom = require('jsdom').jsdom;
- var document = jsdom('<html></html>', {});
- var window = document.defaultView;
- var $ = require('jquery')(window);
- var categorias = [];
+var document = jsdom('<html></html>', {});
+var window = document.defaultView;
+var $ = require('jquery')(window);
+var categorias = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
