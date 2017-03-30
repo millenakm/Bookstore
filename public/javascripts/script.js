35dcd837a62c7c8b64a5c7a1bf23697b4f81e038
Millena Kupsinsku Martins <millena.martins@digitaldesk.com.br>
2017-03-30 15:55:51 -0300

    carrinho

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,5 @@
 var tallest = 0;
+var valor = 0;
 	
 // Abre a tela de pesquisa
 function openSearch(){
@@ -17,16 +18,16 @@ function closeSearch(){
 }
 
 // Muda caracteristicas conforme a scrollbar
-function offSetManager(){
-	var yOffset = 0;
-	var currYOffSet = window.pageYOffset;
-	if(yOffset < currYOffSet) {
+function onScroll(){
+	var scrollY = 0;
+	var pageScroll = window.pageYOffset;
+	if(scrollY < pageScroll) {
 		$('.navig').addClass('navig-top');
 		$('.cart-box').addClass('cart-top');
 		$('.navbar-content').css({top: '15%'});
 		$('#pg-title').fadeOut(200);
 	}
-	else if(currYOffSet == yOffset){
+	else if(pageScroll == scrollY){
 		$('.navig').removeClass('navig-top');
 		$('.cart-box').removeClass('cart-top');
 		$('.navbar-content').css({top: '10%'});
@@ -35,14 +36,16 @@ function offSetManager(){
 }
 
 // muda a altura das divs .grid, para todas da mesma altura
-function equalHeight(group) {
-	group.each(function() {
+function equalHeight(group){
+	group.each(function(){
 		var thisHeight = $(this).height();
 		if(thisHeight > tallest) {
 			tallest = thisHeight;
 		}
 	});
-	group.each(function() { $(this).height(tallest); });
+	group.each(function(){ 
+		$(this).height(tallest); 
+	});
 } 
 
 // faz a busca dos dados
@@ -50,31 +53,35 @@ function searchJson(){
 	$('#search-input').keyup(function(){//quando escrever na input, realiza a busca
 		var searchField = $(this).val();
 		if(searchField === '')  {//se não houver nada no campo de busca, esconde a div de resultados
-			$(".list-result").html('');
+			$(".list-result, #box-result").html('');
 			return;
 		}
 
-		var output = '';//cria a div para mostras os resultados
+		var results = '';//cria a div para mostras os resultados
 		var regex = new RegExp(searchField, "i");//define a busca sem case sensitive
-		$.get("http://localhost:3000/dados", function(data){//procura os dados
+		$.get("/dados", function(data){//procura os dados
 			var cor = 0;
 			for(i in data){
 				var titulo = data[i].titulo;
 				if (titulo.search(regex) != -1 || data[i].autor.search(regex) != -1 || data[i].editora.search(regex) != -1){//se houver dados correspondentes à busca
 					//mostra a div de resultados e printa 
-					output+='<a><div id="livros" class="box-book" data-id="'+data[i].isbn+'" onmouseover="painelResult(this)"><div id="livros-result" class="row livrosCor-'+cor+'">'
+					results+='<a><div id="livros" class="box-book" data-id="'+data[i].isbn+'" onmouseover="painelResult(this)"><div id="livros-result" class="row livrosCor-'+cor+'">'
 					+'<h5 class="col-md-3">'+data[i].titulo+'</h5><h5 class="col-md-3">'+data[i].autor+'</h5>'
-					+'<h3 class="col-md-3">R$ '+data[i].preço.toString()+'</h3></a></div></div>';
+					+'<h4 class="col-md-3">R$ '+data[i].preço.toString()+'</h4>'
+					+'<h5 class="col-md-3">Editora: '+data[i].editora+'</h5></a></div></div>';
 
 					cor++;
 				}
 			}
-			output += '';
-			$('.list-result').html(output);//define a div com os resultados
+			results += '';
+			$('.list-result').html(results);//define a div com os resultados
 			$('.box-book').on('click', function(){
 				$(".search-modal").animate({display: 'none'});
 				product($(this).data('id'));
-			}); 		
+			}); 
+			if($(".list-result").height()==1){
+				$(".list-result").html("<h2>Nenhum resultado encontrado.</h2>");
+			}		
 		});
 	});
 }
@@ -94,16 +101,15 @@ function painelResult(elem){
 // vai para a página do produto clicado
 function product(parameters){
 	window.location=('/catalogo/produto?cod='+parameters);
-
-};
-
-// box do carrinho
-function cart(){
-    // var state = parseInt($('.cart-box').css('height')) > 1;
-    // $('.cart-box').animate({height:(state ? "30%" : '0')}, 100);
-    // $('.cart-box').animate({height:(state ? '0': '30%')}, 200);
-    $(".cart-box").slideToggle('slow');
 }
+
+// // box do carrinho
+// function cart(){
+//     // var state = parseInt($('.cart-box').css('height')) > 1;
+//     // $('.cart-box').animate({height:(state ? "30%" : '0')}, 100);
+//     // $('.cart-box').animate({height:(state ? '0': '30%')}, 200);
+//     $(".cart-box").slideToggle('slow');
+// }
 
 // link ativo
 function activeLink(){
@@ -117,7 +123,7 @@ function activeLink(){
 // ações
 function actions(){
 	window.onscroll = function() {
-		offSetManager();
+		onScroll();
 	}
 	$('#open-search').click(function(){
 		openSearch();
@@ -132,29 +138,44 @@ function actions(){
 		activeLink();
 	});
 	$('.box-book').on('click', function(){
-		product($(this).parents('.grid').data('id'));
+		product($(this).parents('.isbn').data('id'));
 	});
 	$('.heart').click(function(){
-		var param = {add: $(this).parents('.grid').data('id'), op: 'adicionar'};
+		var param = {add: $(this).parents('.isbn').data('id'), op: 'adicionar'};
 		heart(this, param);
 	});
 	$('.cart').click(function(){
-		var param = {add: $(this).parents('.grid').data('id'), op: 'adicionar'};
-		carrinho(this, param);
+		var param = {add: $(this).parents('.isbn').data('id'), op: 'adicionar'};
+		cart(this, param);
+	});
+	$('.deletar').click(function(){
+		cartProducts(this);
 	});
 	$('#filter').change(function(){
 		filter($(this).val());
 	});
 	$(".heart").each(function() {
-		var param = {add: $(this).parents('.grid').data('id'), op: 'listar'};
+		var param = {add: $(this).parents('.isbn').data('id'), op: 'listar'};
 		heart(this, param);
 	});
 	$(".cart").each(function() {
-		var param = {add: $(this).parents('.grid').data('id'), op: 'listar'};
-		carrinho(this, param);
+		var param = {add: $(this).parents('.isbn').data('id'), op: 'listar'};
+		cart(this, param);
 	});
 	$("#select").change(function(){
 		filter(this);	
+	});
+	$('.removeDesejo').on('click', function(){
+		$(this).parents('.grid').fadeOut();
+	})
+}
+
+function cartProducts(elem){
+	$(elem).parents('.isbn').addClass('excluir');
+	$('.excluir').hide(function(){ 
+		$('.excluir').remove(); 
+		valor = 0;
+		totalValue($(".valor"));
 	});
 }
 
@@ -186,7 +207,7 @@ function heart(elem, param){
 function filter(elem){
 	var categoria = $(elem).val();
 	$('.grid').hide();
-	$('.mensagem').hide();
+	$('#mensagem').hide();
 	$('.grid').each(function(){
 		if($(this).data('categ')==categoria){
 			$(this).show();
@@ -196,12 +217,12 @@ function filter(elem){
 		$('.grid').show();
 	}
 	if($('.grid:visible').length==0){
-		$('.mensagem').show();
+		$('#mensagem').show();
 	}
 }
 
 // carrinho
-function carrinho(elem, param) {
+function cart(elem, param) {
 	$.get( '/carrinho', param, function(data){
 		if(data == true){
 			$(elem).css({'color':'aqua'});
@@ -226,21 +247,40 @@ function carrinho(elem, param) {
 }
 
 // valor total do carrinho
-function valorTotal(group) {
+function totalValue(group) {	
 	group.each(function() {
-		var thisHeight = $(this).data('valor');
+		valor+=Number($(this).data('valor').replace(',','.'));
 	});
-	group.each(function() { $(this); });
+	$('.valorTotal').html('<h3>Total: R$ '+valor.toFixed(2).toString().replace('.',',')+'</h3>');
 } 
 
+function categFilter(){
+	var categorias = [];
+	$.get('/dados', function(data){
+		$.each(data, function(index, value) {
+		    if ($.inArray(value.categoria, categorias)==-1) {
+		        categorias.push(value.categoria);
+		    }
+		});
+		for(i in categorias){
+			$("#select").append('<option value="'+categorias[i]+'">'+categorias[i]+'</option>');
+		}
+	});
+}
+
+function removeDesejos(elem){
+	$(elem).find('.heart').addClass('removeDesejo');
+}
 
 $(document).ready(function(){ 
 	$("body").fadeIn(500);
-	$('.mensagem').hide();
-	offSetManager();
+	removeDesejos($("#desejos"));
+	$('#mensagem').hide();
+	onScroll();
 	actions();
-	valorTotal($(".valor"));
+	totalValue($(".valor"));
 	equalHeight($(".grid")); 
 	searchJson();
+	categFilter();
 	filter($("#select"));
 });
--- routes/desejo.js
+++ routes/desejo.js
@@ -33,7 +33,7 @@ router.get('/', function(req, res, next) {
 		}
 		else{
 			file.read(function(dadosGerais){
-				res.render('./catalogo/desejos', {title: "Desejos", dados: data, total: dadosGerais});
+				res.render('./catalogo/desejos', {title: "Desejos", desejados: data, dados: dadosGerais});
 				res.end();
 			});	
 		}
